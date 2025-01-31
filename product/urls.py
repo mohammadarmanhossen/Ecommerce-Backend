a98a2964b140192c56500ee01c4e30d816bad946
arman <mdarmanhossen404@gmail.com>
2025-01-31 23:23:17 +0600

    Ecommerce-Backend

---
+++ product/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('product',views.ProductViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+if settings.DEBUG:  # Serve media files only in development
+ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
