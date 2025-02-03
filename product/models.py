a2e05acad7ad08bcbc9fd9f1e78be9f604e0b147
arman <mdarmanhossen404@gmail.com>
2025-02-04 01:52:49 +0600

    Ecommerce backend

--- product/models.py
+++ product/models.py
@@ -15,20 +15,20 @@ class Product(models.Model):
         ordering = ['-price',]
 
 STAR_CHOICES = [
-        ('⭐', '⭐'),
-        ('⭐⭐', '⭐⭐'),
-        ('⭐⭐⭐', '⭐⭐⭐'),
-        ('⭐⭐⭐⭐', '⭐⭐⭐⭐'),
-        ('⭐⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'),
+    (1, '⭐'),
+    (2, '⭐⭐'),
+    (3, '⭐⭐⭐'),
+    (4, '⭐⭐⭐⭐'),
+    (5, '⭐⭐⭐⭐⭐'),
 ]
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     email = models.EmailField()
-    star = models.IntegerField(choices=STAR_CHOICES)
+    star = models.IntegerField(choices=STAR_CHOICES)  # Now it correctly stores an integer
     body = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
-        return f"{self.user.username} rate {self.product.name}"
+        return f"{self.user.username} rated {self.product.name} {dict(STAR_CHOICES)[self.star]}"
